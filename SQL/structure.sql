1cef003ea50cfd0bce5634b9b1157744f07cace6
Edgars Pelna <regative@gmail.com>
2012-08-30 09:38:08 +0300

    Added SQL DB structure of tables «categories»

---
+++ SQL/structure.sql
@@ -0,0 +1,33 @@
+-- Adminer 3.5.1 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  `topics_count` int(11) NOT NULL,
+  `admin_rule` int(11) NOT NULL,
+  `moderator_rule` int(11) NOT NULL,
+  `user_rule` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` text NOT NULL,
+  `email` text NOT NULL,
+  `password` text NOT NULL,
+  `picture` text NOT NULL,
+  `user_role` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2012-08-28 17:25:53
