038fbfb87b04bc42198e87b6251952bc05986f70
Edgars Pelna <regative@gmail.com>
2012-08-30 17:29:34 +0300

    Now user`s signature will be parsed with markdown

--- application/views/profile/view.php
+++ application/views/profile/view.php
@@ -1,6 +1,7 @@
 <?php foreach ($users as $user): ?>
 	<h2><?php echo $user->username; ?>'s profile:</h2>
+	<h3>Avatar (<a href="<?php echo URL::site('profile/upload_avatar'); ?>">edit</a>)</h3>
 	<img src="<?php echo $user->picture; ?>" height="120px" align="left" />
-	<h3>Signature:</h3>
-	<?php echo $user->signature; ?>
+	<h3>Signature (<a href="<?php echo URL::site('profile/change_signature'); ?>">edit</a>):</h3>
+	<?php echo Darkmown::parse($user->signature); ?>
 <?php endforeach; ?>
