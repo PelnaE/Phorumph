0ea8dfda432440e97780f0ccf826d969e1c2dbf6
Edgars Pelna <regative@gmail.com>
2012-08-30 10:53:34 +0300

    Added simple login form in header

--- application/views/theme/default.php
+++ application/views/theme/default.php
@@ -7,6 +7,13 @@
 <body>
 	<header>
 		<h1><a href="<?php echo URL::site(''); ?>"><?php echo $site_name; ?></a></h1>
+		<h2>Login:</h2>
+		<form action="<?php echo URL::site('login/'.Security::token()); ?>" method="post">
+			Username <input type="text" name="username" /><br />
+			Password <input type="password" name="password" /><br />
+			<input type="submit" value="Login" />
+		</form>
+		If you do not have an account, <a href="<?php echo URL::site("register"); ?>">register it there</a>!
 	</header>
 	<?php if (!isset($content)): ?>
 		<h2>No View is set</h2>
