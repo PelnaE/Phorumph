faae6ad2a3b6a3a4624c25f560646824649f6faf
Edgars Pelna <regative@gmail.com>
2012-09-21 18:49:20 +0300

    Added link to password`s recovery form in header.

--- application/views/theme/default.php
+++ application/views/theme/default.php
@@ -39,7 +39,9 @@
 		<?php else: ?>
 			<h2>Login:</h2>
 			<form action="<?php echo URL::site('login/index/'.Security::token()); ?>" method="post">
-				Username <input type="text" name="username" /><br />
+				Username <input type="text" name="username" />
+				<a href="<?php echo URL::site('forgot_password/'); ?>">Forgot Password?</a>
+				<br />
 				Password <input type="password" name="password" /><br />
 				<label><input type="checkbox" name="cookie" /> Remember me!</label><br />
 				<input type="submit" value="Login" />
