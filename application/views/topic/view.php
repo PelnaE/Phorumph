97159b69a725b0a11d7bb54430c2b49102ff0a96
Edgars Pelna <regative@gmail.com>
2012-09-23 12:55:50 +0300

    Added «Back» button in topic`s title.

--- application/views/topic/view.php
+++ application/views/topic/view.php
@@ -1,6 +1,12 @@
 
 <?php foreach ($topics as $topic): ?>
-<h3><?php echo HTML::chars($topic->title); ?></h3>
+<h3>
+    <a href="<?php echo URL::site('category/index/'.$topic->category_id); ?>">
+        Back
+    </a>
+    &mdash;
+    <?php echo HTML::chars($topic->title); ?>
+</h3>
 <table width="40%" border="1px" cellspacing="1" cellpadding="5">
     <tr>
         <td width="10%">
@@ -8,7 +14,7 @@
             <img src="<?php echo $topic->picture; ?>" height="80px" />
         </td>
         <td valign="top">
-            <?php echo date('d.m.Y H:i:s', $topic->published); ?><br />
+            <?php echo date("d.m.Y H:i:s", $topic->published); ?><br />
             <?php echo $topic->content; ?>
         </td>
     </tr>
