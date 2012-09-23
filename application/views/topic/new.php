955aa907528b2e312afeefa3a3639125f4f486c8
Edgars Pelna <regative@gmail.com>
2012-09-23 15:54:13 +0300

    Added a label at the category select box.

--- application/views/topic/new.php
+++ application/views/topic/new.php
@@ -2,7 +2,7 @@
 <form action="<?php echo URL::site('topic/new/'.Security::token()); ?>" method="post">
     Topic title: <input type="text" name="title" /><br />
     <?php if (isset($categories)): ?>
-        <select name="category_id">
+       Category:  <select name="category_id">
             <?php foreach ($categories as $category): ?>
                 <option value="<?php echo $category->id ?>"><?php echo $category->name ?></option>
             <?php endforeach; ?>
