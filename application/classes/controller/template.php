e8dfdf521f8b9b4d113edf2b6816cb728717896a
reGative <regative@gmail.com>
2013-02-20 17:34:28 +0200

    Refactoring template.php view.

--- application/classes/controller/template.php
+++ application/classes/controller/template.php
@@ -10,8 +10,9 @@ class Controller_Template extends Kohana_Controller_Template
 		parent::before();
 		if (Auth::instance()->logged_in()) {
 			$user                  = new Model_User();
-			$this->template->users    = $user->where('id', '=', Auth::instance()->get_user()->pk())->find();
-			$this->template->users_levels       = $user->get_level(Session::instance()->get('user_id'));
+			$this->template->header = View::factory('profile/header')
+			->set('users', $user->where('id', '=', Auth::instance()->get_user()->pk())->find())
+			->set('users_levels', $user->get_level(Session::instance()->get('user_id')));
 		}
 		$this->template->site_name = $config->site_name;
 	}
--- application/views/theme/default.php
+++ application/views/theme/default.php
@@ -19,45 +19,45 @@
 	</style>
 </head>
 <body>
+
 	<header>
-		<h1><a href="<?php echo URL::site(''); ?>"><?php echo $site_name; ?></a></h1>
-		<?php if (Auth::instance()->logged_in()) : ?>
-				<?php if (!empty($users->picture)) : ?>
-					<img src="<?php echo $users->picture; ?>" height="50px" align="left" />
-				<?php endif; ?>
-				<h2>
-					Hey, <a href="<?php echo URL::site('profile/view/'.$users->id); ?>"><?php echo HTML::chars($users->username); ?></a>!
-					(<a href="<?php echo URL::site('logout/index/'.Security::token()); ?>">Logout</a>)
-				</h2>
-				Today is <?php echo date('l', time()); ?>, the
-				<?php echo date('W', time()); ?> week of the year
-				<?php echo date('Y', time()); ?>!
-				Or, simpler - <?php echo date('d.m.Y H:i:s T', time()); ?>
-		<?php else: ?>
-			<h2>Login:</h2>
-			<form action="<?php echo URL::site('login/index/'.Security::token()); ?>" method="post">
-				Username <input type="text" name="username" />
-				<a href="<?php echo URL::site('forgot_password/'); ?>">Forgot Password?</a>
-				<br />
-				Password <input type="password" name="password" /><br />
-				<label><input type="checkbox" name="cookie" /> Remember me!</label><br />
-				<input type="submit" value="Login" />
-			</form>
-			If you do not have an account, <a href="<?php echo URL::site("register"); ?>">register it there</a>!
-		<?php endif; ?>
+
+		<h1>
+			<a href="<?php echo URL::site(''); ?>">
+				<?php echo $site_name; ?>
+			</a>
+		</h1>
+
+		<?php echo $header; ?>
+
 	</header>
-    <?php if (Auth::instance()->logged_in()): ?>
-        <h3><a href="<?php echo URL::site('topic/new'); ?>">Start a new discussion.</a></h3>
-    <?php endif; ?>
+
+	<?php if (Auth::instance()->logged_in()): ?>
+
+		<h3>
+			<a href="<?php echo URL::site('topic/new'); ?>">Start a new discussion.</a>
+		</h3>
+
+	<?php endif; ?>
+
 	<?php if (!isset($content)): ?>
+
 		<h2>No View is set</h2>
 		<p>Contact administrator.</p>
+
 	<?php else: ?>
+
 		<?php echo $content; ?>
+
 	<?php endif; ?>
+
 	<footer>
+
 		Powered by Phorumph Discussion Board.
 		&copy; By reGative. 2012.
+
 	</footer>
+
 </body>
+
 </html>
