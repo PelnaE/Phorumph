4badd7915aa2187fad430eefb9f1b2a584141e69
reGative <regative@gmail.com>
2013-02-24 14:28:04 +0200

    Formatting list.php

--- application/views/dashboard/users/list.php
+++ application/views/dashboard/users/list.php
@@ -1,17 +1,17 @@
 <h3>List of Users</h3>
 <table>
-    <tr>
-        <th>ID</th>
-        <th>Picture</th>
-        <th>Username</th>
-        <th>e-mail</th>
-    </tr>
-    <?php foreach ($users as $user): ?>
-    <tr>
-        <td><?php echo $user->id; ?></td>
-        <td><?php echo $user->picture; ?></td>
-        <td><?php echo $user->username; ?></td>
-        <td><?php echo $user->email; ?></td>
-    </tr>
-    <?php endforeach; ?>
+	<tr>
+		<th>ID</th>
+		<th>Picture</th>
+		<th>Username</th>
+		<th>e-mail</th>
+	</tr>
+	<?php foreach ($users as $user): ?>
+	<tr>
+		<td><?php echo $user->id; ?></td>
+		<td><?php echo $user->picture; ?></td>
+		<td><?php echo $user->username; ?></td>
+		<td><?php echo $user->email; ?></td>
+	</tr>
+	<?php endforeach; ?>
 </table>
