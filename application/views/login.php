0984ca7e1416bb5ff2fc74afad96725d815ad3cc
reGative <regative@gmail.com>
2013-02-24 19:00:11 +0200

    New style for login-form.

--- application/views/login.php
+++ application/views/login.php
@@ -1,9 +1,18 @@
 <h2>Login:</h2>
-<form action="<?php echo URL::site('login/index/'.Security::token()); ?>" method="post">
-	Username <input type="text" name="username" /><br />
-	Password <input type="password" name="password" /><br />
-	<label><input type="checkbox" name="cookie" /> Remember me!</label><br />
-	<input type="submit" value="Login" />
-</form>
 <a href="<?php echo URL::site("register"); ?>">Register</a> |
 <a href="<?php echo URL::site('forgot_password/'); ?>">Forgot Password?</a>
+
+<form action="<?php echo URL::site('login/index/'.Security::token()); ?>" method="post">
+    <table>
+        <tr>
+            <td>Username</td>
+            <td><input type="text" name="username" /></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="password" /></td>
+        </tr>
+    </table>
+	<label><input type="checkbox" name="cookie" /> Remember me!</label>
+	<input type="submit" value="Login" />
+</form>
